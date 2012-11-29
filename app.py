3f3b62e1db2dda649383026cab9baf6ce24244f9
Matt Tarbit <mtarbit@gmail.com>
2012-11-29 22:16:48 +0000

    Just something to start off with.

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
+
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    # Bind to port if defined, otherwise default to 5000.
     host = '0.0.0.0'
     port = int(os.environ.get('PORT', 5000))
     app.run(host=host, port=port)
